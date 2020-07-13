1e8e1421c022a4b02749e7bb4d2425c280d936ca
Makan Taghizadeh <makan@ngart.co>
2020-07-13 22:12:59 +0200

    Create Xcode project

---
+++ OTPWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OTPWallet
+//
+//  Created by Makan Taghizadeh on 13/07/2020.
+//  Copyright © 2020 ngArt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
